8c2dfa4dead972a140600ca7ea279ae7d04d0604
Subin Cho <sbjo7713@gmail.com>
2022-05-13 11:10:57 +0900

    매개변수 전달 방법 알아보기

--- src/test/java/hello/aop/pointcut/AtAnnotationTest.java
+++ src/test/java/hello/aop/pointcut/AtAnnotationTest.java
@@ -1,7 +1,6 @@
 package hello.aop.pointcut;
 
 import hello.aop.member.MemberService;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -35,6 +34,5 @@ public class AtAnnotationTest {
             return joinPoint.proceed();
         }
     }
-
 
 }
